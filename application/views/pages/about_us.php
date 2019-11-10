d923a674c05e033eec9fa376e9ba156044d2d37d
Kevin Walker <kevin.walker@droid.rocks>
2019-11-09 21:01:04 -0500

    Set up contact form for About Us page. Added contact info and Google Map to show location

--- application/views/pages/about_us.php
+++ application/views/pages/about_us.php
@@ -1,1 +1,100 @@
-Martha's Brew About Us
+<section class="section pb-5">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <!--Section heading-->
+    <!-- Font Awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+
+    <h2 class="section-heading h1 pt-4">About Martha's Brew</h2>
+    <!--Section description-->
+    <p class="section-description pb-4">Since October 2019, we at Martha's Brew have strived to provide to best
+    quality service in the area. We know how much you folks at Data Management love your coffee, so if our service does
+    not meet your standards, please don't hesitate to reach out!</p>
+
+    <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-5 mb-4">
+
+            <!--Form with header-->
+            <div class="card">
+
+                <div class="card-body">
+                    <!--Header-->
+                    <div class="form-header blue accent-1">
+                        <h3><i class="fas fa-envelope"></i> Reach out:</h3>
+                    </div>
+
+                    <br>
+
+                    <!--Body-->
+                    <div class="md-form">
+                        <i class="fas fa-user prefix grey-text"></i>
+                        <input type="text" id="form-name" class="form-control">
+                        <label for="form-name">Your name</label>
+                    </div>
+
+                    <div class="md-form">
+                        <i class="fas fa-envelope prefix grey-text"></i>
+                        <input type="text" id="form-email" class="form-control">
+                        <label for="form-email">Your email</label>
+                    </div>
+
+                    <div class="md-form">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <input type="text" id="form-Subject" class="form-control">
+                        <label for="form-Subject">Subject</label>
+                    </div>
+
+                    <div class="md-form">
+                        <a class="btn-floating blue accent-1"><i class="fas fa-envelope"></i></i></a>
+                        <textarea id="form-text" class="form-control md-textarea" rows="3"></textarea>
+                        <label for="form-text">Message</label>
+                    </div>
+
+                    <div class="text-center">
+                        <button type="button" class="btn btn-primary">SEND</button>
+                    </div>
+                </div>
+            </div>
+            <!--Form with header-->
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-7">
+
+            <!--Google map-->
+            <div id="map-container-google-11" class="z-depth-1-half map-container-6" style="height: 400px">
+                <iframe src="https://maps.google.com/maps?width=600%&amp;height=600&amp;hl=en&amp;coord=36.462939,
+                -79.906767&amp;q=100%20Mebane%20Street%2C%20Stoneville%2C%20NC+(Martha's%20Brew)&amp;ie=UTF8&amp;t=&amp;
+                z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    <a href="https://www.maps.ie/coordinates.html">gps coordinates finder</a></iframe>
+            </div>
+
+            <br>
+            <!--Buttons-->
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <a class="btn-floating blue accent-1"><i class="fas fa-map-marker-alt"></i></a>
+                    <p>Stoneville, NC 27048</p>
+                    <p>United States</p>
+                </div>
+
+                <div class="col-md-4">
+                    <a class="btn-floating blue accent-1"><i class="fas fa-phone"></i></a>
+                    <p>+1 336.269.5650</p>
+                    <p>Mon - Sat, 7AM - 7PM</p>
+                </div>
+
+                <div class="col-md-4">
+                    <a class="btn-floating blue accent-1"><i class="fas fa-envelope"></i></a>
+                    <p>marthasbrew@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <!--Grid column-->
+    </div>
+</section>
