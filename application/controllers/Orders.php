fe529e2da099d60b533215c5691fa30fd54a5c4e
Kevin Walker <kevin.walker@droid.rocks>
2019-12-01 19:32:29 -0500

    Modified Orders page. Changes w/Trent

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -3,21 +3,51 @@ class Orders extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('orders_model');
+        $this->load->model('Items_model');
+        $this->load->model('Orders_model');
+        $this->load->model('Order_items_model');
         $this->load->helper('url_helper');
     }
 
     public function index() {
-        $data['orders'] = $this->orders_model->get_orders();
-        $data['title'] = 'Order Food';
+        $data['hot_items'] = $this->Items_model->get('HOT');
+        $data['cold_items'] = $this->Items_model->get('COLD');
 
+        // Set title
+        $data['title'] = 'Order Coffee';
+
+        // Load template header
         $this->load->view('templates/header', $data);
+
+        // Load order page
         $this->load->view('orders/index', $data);
+
+        // Load template footer
         $this->load->view('templates/footer');
     }
 
+    public function submit() {
+
+        $customer_name = $this->input->post('customer_name');
+        $phone_number = $this->input->post('phone_number');
+        $items = $this->input->post('items');
+
+        $cost = 0;
+        foreach($items as $item_id) {
+            $cost = $cost + $this->Items_model->getCost($item_id);
+        }
+
+        $order_id = $this->Orders_model->create($customer_name,$phone_number,$cost);
+
+        foreach($items as $item_id) {
+            $this->Order_items_model->create($order_id, $item_id);
+        }
+
+        echo "Your order is complete!";
+    }
+
     public function view($id = NULL) {
-        $data['order_item'] = $this->order_model->get_orders($id);
+        //$data['order_item'] = $this->Orders_model->get_orders($id);
 
         if(empty($data['order_item'])) {
             show_404();
