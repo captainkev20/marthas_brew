2a4ff4392e50b55d4b701d7ebc95b333d96ae08b
Kevin Walker <kevin.walker@droid.rocks>
2019-12-03 14:00:10 -0500

    Modified Orders_model so you can get create/update orders and retrieve based on status. Changes w/Trent.

--- application/models/Orders_model.php
+++ application/models/Orders_model.php
@@ -1,15 +1,43 @@
 <?php
 class Orders_model extends CI_Model {
 
-    public function __construct() {
-        $this->load->database();
+    public function get_order($id) {
+        $this->db->where('id',$id);
+        $query = $this->db->get('orders');
+        return $query->row_array();
     }
 
-    public function get_orders($id = FALSE) {
-        if($id == FALSE) {
-            $query = $this->db->get('items');
-            return $query->result_array();
-        }
+    public function get_pending() {
+        $this->db->where('status','pending');
+        $query = $this->db->get('orders');
+        return $query->result_array();
+    }
 
+    public function get_paid() {
+        $this->db->where('status','paid');
+        $query = $this->db->get('orders');
+        return $query->result_array();
+    }
+
+    public function create($customer_name, $phone_number, $cost) {
+        $order = array(
+            'customer_name' => $customer_name,
+            'phone_number' => $phone_number,
+            'status' => 'pending',
+            'cost' => $cost
+        );
+
+        $this->db->insert('orders',$order);
+
+        return $this->db->insert_id();
+    }
+
+    public function update($id, $status) {
+        $order = array(
+            'status' => $status
+        );
+
+        $this->db->where('id',$id);
+        $this->db->update('orders',$order);
     }
 }
