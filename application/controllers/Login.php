8a6aa547eee3456633ce930edddc7829ff549562
Kevin Walker <kevin.walker@droid.rocks>
2019-12-06 06:18:04 -0500

    Login Controller - Added url helper and form_validation library. Added function to validate user and redirect to order status page upon login.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,11 +5,12 @@ class Login extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('Login_model');
+        $this->load->helper('url');
+        $this->load->library('form_validation');
+
     }
 
     public function index() {
-
-        $data['users'] = $this->Login_model->authenticateUser('kevin_walker','123456789');
 
         // Set title
         $data['title'] = 'Employee Login';
@@ -22,5 +23,22 @@ class Login extends CI_Controller {
 
         // Load template footer
         $this->load->view('templates/footer');
+    }
+
+    public function loginUser() {
+
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+        if($this->form_validation->run()) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            if($this->Login_model->authenticateUser($username,$password)) {
+                redirect('http://localhost:8888/marthas_brew/orderstatus', 'refresh');
+            } else {
+                echo 'Sorry, your information is incorrect.';
+            }
+        }
     }
 }
